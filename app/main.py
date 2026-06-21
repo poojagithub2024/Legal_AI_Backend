6967914fbda8af90f3b51744d27b11c22366f6a6
poojagithub2024 <poojaselvam1785@gmail.com>
2026-06-21 14:11:05 +0530

    Updated project configuration

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from app.models.database import engine, Base
 from app.models.document import Document
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routes.upload import router as upload_router
 from app.routes.extract import router as extract_router
@@ -11,7 +12,13 @@ from app.routes.simulate import router as simulate_router
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(upload_router)
 app.include_router(extract_router)
 app.include_router(analyze_router)
